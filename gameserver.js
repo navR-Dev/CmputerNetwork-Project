b743b7bdfd96bad0c25913cf438bf5ebce9cbf25
naveenRDev <114591918+naveenRDev@users.noreply.github.com>
2024-02-18 17:49:33 +0530

    Added code and ssl documents

---
+++ gameserver.js
@@ -0,0 +1,143 @@
+// A server for Tic-tac toe games.
+//
+// The first two client connections become X and O for the first game; the next
+// two connections face off in the second game, and so on. Games run concurrently.
+//
+// The games use TTTP, the "Tic Tac Toe Protocol" which I just made up:
+//
+// Client -> Server
+//     MOVE <n>
+//     QUIT
+//
+// Server -> Client
+//     WELCOME <char>
+//     VALID_MOVE
+//     OTHER_PLAYER_MOVED <n>
+//     OTHER_PLAYER_LEFT
+//     VICTORY
+//     DEFEAT
+//     TIE
+//     MESSAGE <text>
+//
+// The cells are numbered top-to-bottom, left-to-right, as 0..8.
+
+const net = require("net");
+const fs = require("fs");
+const tls = require("tls");
+
+let game = null;
+const options = {
+  key: fs.readFileSync("server-key.pem", "utf8"), // Read key as UTF-8 string
+  cert: fs.readFileSync("server-cert.pem"),
+  passphrase: "naveen", // Provide your passphrase here
+};
+
+const server = tls
+  .createServer(options, (socket) => {
+    console.log(
+      "Connection from",
+      socket.remoteAddress,
+      "port",
+      socket.remotePort
+    );
+    if (game === null) {
+      game = new Game();
+      game.playerX = new Player(game, socket, "X");
+    } else {
+      game.playerO = new Player(game, socket, "O");
+      game = null;
+    }
+  })
+  .listen(58901, () => {
+    console.log("Tic Tac Toe Server is Running");
+  });
+
+class Game {
+  // A board has nine squares. Each square is either unowned or it is owned by a
+  // player. So we use a simple array of player references. If null, the corresponding
+  // square is unowned, otherwise the array cell stores a reference to the player that
+  // owns it.
+  constructor() {
+    this.board = Array(9).fill(null);
+  }
+
+  hasWinner() {
+    const b = this.board;
+    const wins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    return wins.some(
+      ([x, y, z]) => b[x] !== null && b[x] === b[y] && b[y] === b[z]
+    );
+  }
+
+  boardFilledUp() {
+    return this.board.every((square) => square !== null);
+  }
+
+  move(location, player) {
+    if (player !== this.currentPlayer) {
+      throw new Error("Not your turn");
+    } else if (!player.opponent) {
+      throw new Error("You don’t have an opponent yet");
+    } else if (this.board[location] !== null) {
+      throw new Error("Cell already occupied");
+    }
+    this.board[location] = this.currentPlayer;
+    this.currentPlayer = this.currentPlayer.opponent;
+  }
+}
+
+class Player {
+  constructor(game, socket, mark) {
+    Object.assign(this, { game, socket, mark });
+    this.send(`WELCOME ${mark}`);
+    if (mark === "X") {
+      game.currentPlayer = this;
+      this.send("MESSAGE Waiting for opponent to connect");
+    } else {
+      this.opponent = game.playerX;
+      this.opponent.opponent = this;
+      this.opponent.send("MESSAGE Your move");
+    }
+
+    socket.on("data", (buffer) => {
+      const command = buffer.toString("utf-8").trim();
+      if (command === "QUIT") {
+        socket.destroy();
+      } else if (/^MOVE \d+$/.test(command)) {
+        const location = Number(command.substring(5));
+        try {
+          game.move(location, this);
+          this.send("VALID_MOVE");
+          this.opponent.send(`OPPONENT_MOVED ${location}`);
+          if (this.game.hasWinner()) {
+            this.send("VICTORY");
+            this.opponent.send("DEFEAT");
+          } else if (this.game.boardFilledUp()) {
+            [this, this.opponent].forEach((p) => p.send("TIE"));
+          }
+        } catch (e) {
+          this.send(`MESSAGE ${e.message}`);
+        }
+      }
+    });
+
+    socket.on("close", () => {
+      try {
+        this.opponent.send("OTHER_PLAYER_LEFT");
+      } catch (e) {}
+    });
+  }
+
+  send(message) {
+    this.socket.write(`${message}\n`);
+  }
+}
